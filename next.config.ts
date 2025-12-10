edf407072af480c63ab0d65055e19c0f5c825efb
unknown <ahmedshawqi@dubisign.com>
2025-12-10 15:54:54 +0300

    enhance projects

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin("./i18n.ts");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
